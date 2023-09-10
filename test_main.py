54e08ca5a256cca15e95a761203b3e5b21ca3603
Ben Schwartz <btschwartz12@gmail.com>
2023-09-10 00:43:42 -0400

    Trying on self host

--- test_main.py
+++ test_main.py
@@ -1,10 +1,12 @@
 # test_main.py
+import time
 import unittest
 from main import add
 
 class TestMain(unittest.TestCase):
 
     def test_add(self):
+        time.sleep(10)
         self.assertEqual(add(3, 4), 7)
 
 if __name__ == "__main__":
