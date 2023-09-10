2276416e96c5a6e4dec619011e9380326b82e1c4
Ben Schwartz <btschwartz12@gmail.com>
2023-09-10 00:41:09 -0400

    bruh

---
+++ bad.sh
@@ -0,0 +1,20 @@
+echo "bruh" >> bruh.txt
+git add .
+git commit -m "bruh"
+git push
+echo "bruh" >> bruh.txt
+git add .
+git commit -m "bruh2"
+git push
+echo "bruh" >> bruh.txt
+git add .
+git commit -m "bruh3"
+git push
+echo "bruh" >> bruh.txt
+git add .
+git commit -m "bruh4"
+git push
+echo "bruh" >> bruh.txt
+git add .
+git commit -m "bruh5"
+git push
